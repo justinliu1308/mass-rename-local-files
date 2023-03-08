aa6f641eb8459094cda5ea2e0cdd5e8c20f68ceb
Justin Liu <justinliu1308@gmail.com>
2023-03-08 11:08:20 -0500

    Adding unit testing

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import os, re
-path = ""
+import os
+import re
+
+path = r"C:\Users\justi\Desktop\Python\rename-python-files\old-filenames"
 old_filenames = os.listdir(path)
 new_filenames = []
 
@@ -8,10 +10,12 @@ for name in old_filenames:
     if '_' in name:         # filename is snake_case
         name = name.replace("_", " ")    
     elif '_' not in name:   # filename is UpperCamelCase
-        name = re.sub(r"(\w)([A-Z])", r"\1 \2", name)   # adds space before each capital letter
+        name = re.sub(r"(\w)([A-Z])", r"\1 \2", name)
+        # adds space before each capital letter only if preceeding letter is lowercase
+            # changes PhaseTwo to Phase Two
+            # changes PhaseII to Phase II and not Phase I I
     else:                   # filename format uncertain
-        pass
-    print(name)
+        print(f'The filename "{name}" was not modified due to naming convention not recognized by this program.')
     new_filenames.append(name)
 
 # old filename:
